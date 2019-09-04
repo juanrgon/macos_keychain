d493417ca7fa85475ff80a66e1e53d1b096da702
Juan Gonzalez <jrg2156@gmail.com>
2019-09-04 10:25:03 -0400

    update

--- macos_keychain/main.py
+++ macos_keychain/main.py
@@ -1,11 +1,56 @@
-def add():
-    return 'add'
+import getpass
+import io
 
-def get():
-    return 'get'
+import sh
+
+MACOS_KEYCHAIN_PREFIX = "Python macos_keychain"
+
+
+def add(*, name: str, password: str):
+    out = io.StringIO()
+    sh.security(
+        "add-generic-password",
+        a=getpass.getuser(),
+        s=f"{MACOS_KEYCHAIN_PREFIX}: {name}",
+        w=password,
+        _out=out,
+    )
+    return out.getvalue()
+
+
+def get(*, name: str):
+    out = io.StringIO()
+    sh.security(
+        "find-generic-password",
+        "-w",
+        a=getpass.getuser(),
+        s=f"{MACOS_KEYCHAIN_PREFIX}: {name}",
+        _out=out,
+    )
+    return out.getvalue()
+
 
 def ls():
-    return 'ls'
+    out = io.StringIO()
+    sh.awk(
+        sh.grep(sh.security("dump-keychain"), "0x00000007"),
+        "-F=",
+        "{print $2}",
+        _out=out,
+    )
+    return [
+        n.split(f'"{MACOS_KEYCHAIN_PREFIX}: ')[1][:-1]
+        for n in out.getvalue().strip().split("\n")
+        if n.startswith(f'"{MACOS_KEYCHAIN_PREFIX}: ')
+    ]
 
-def rm():
-    return 'rm'
+
+def rm(name):
+    try:
+        sh.security(
+            "delete-generic-password",
+            a=getpass.getuser(),
+            s=f"{MACOS_KEYCHAIN_PREFIX}: {name}",
+        )
+    except sh.ErrorReturnCode_44:
+        pass
